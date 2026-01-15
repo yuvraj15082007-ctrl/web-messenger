6f83a8d47b205fccc2ece0b8a64d79cb291f232c
Yuvraj <yuvraj15082007@gmail.com>
2026-01-15 13:28:57 +0530

    script.js

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,10 @@
-const socket = io("https://web-messenger-production.up.railway.app", {
-  transports: ["websocket"]
-});
+const socket = io(
+  "https://web-messenger-production.up.railway.app",
+  {
+    path: "/socket.io",
+    transports: ["polling", "websocket"] // polling FIRST
+  }
+);
 
 const messageInput = document.getElementById("messageInput");
 const messagesDiv = document.getElementById("messages");
